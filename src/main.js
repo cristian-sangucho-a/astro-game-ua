f3bf08bc7d7ee2be88c7cec5bc1cc9123a80c22a
Cristtsirc <cristian.sangucho@epn.edu.ec>
2025-01-18 18:06:25 -0500

    feat: add start astro ortografia

--- src/main.js
+++ src/main.js
@@ -1,42 +1,24 @@
-import { Game } from "phaser";
-import { Preloader } from "./preloader";
-import { GameOverScene } from "./scenes/GameOverScene";
-import { HudScene } from "./scenes/HudScene";
-import { MainScene } from "./scenes/MainScene";
-import { MenuScene } from "./scenes/MenuScene";
-import { SplashScene } from "./scenes/SplashScene";
+import Phaser from "phaser";
+import StartAstroOrtografia from "./scenes/StartAstroOrtografia";
+import Preloader from "./Preloader"; // Ajusta la ruta de importación
 
 // More information about config: https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
     type: Phaser.AUTO,
-    parent: "phaser-container",
-    width: 960,
-    height: 540,
-    backgroundColor: "#1c172e",
-    pixelArt: true,
-    roundPixel: false,
-    max: {
-        width: 800,
-        height: 600,
-    },
+    width: 800,
+    height: 500,
     scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+        mode: Phaser.Scale.NONE,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
     },
     physics: {
         default: "arcade",
         arcade: {
-            gravity: { y: 0 }
-        }
+            gravity: { y: 0 },
+        },
     },
-    scene: [
-        Preloader,
-        SplashScene,
-        MainScene,
-        MenuScene,
-        HudScene,
-        GameOverScene
-    ]
+    scene: [StartAstroOrtografia],
 };
 
-new Game(config);
+new Phaser.Game(config);
+
